551b06399a697c38ba894446928ca7b8316a7102
Anh Lê Hữu Tuấn <102230142@sv1.dut.udn.vn>
2025-03-04 22:44:40 +0700

    pushfile

---
+++ DAL/DataBaseAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Net.Http;
+namespace DAL
+{
+    public class SqlConnectionData
+    {
+        static string strCon = " ";//nhap sever SQL vao
+        static SqlConnection sqlCon = null;
+        public static void Connect()
+        {
+            if(sqlCon == null)
+            {
+                SqlConnection sqlCon = new SqlConnection(strCon);
+            }
+        }
+    }
+}
