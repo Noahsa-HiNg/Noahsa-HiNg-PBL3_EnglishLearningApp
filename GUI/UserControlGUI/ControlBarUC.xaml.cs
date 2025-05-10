2f1237fb63954a05ddb8c76e57a188106377649f
Noahsa-HiNg <102230149@sv1.dut.udn.vn>
2025-05-11 03:31:18 +0700

    Add controll bar

---
+++ GUI/UserControlGUI/ControlBarUC.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GUI.ViewModel;
+
+namespace GUI.UserControlGUI
+{
+    /// <summary> 
+    /// Interaction logic for ControlBarUC.xaml
+    /// </summary>
+    public partial class ControlBarUC : UserControl
+    {
+        public ControlBarViewModel Viewmodel { get; set; }
+        public ControlBarUC()
+        {
+            InitializeComponent();
+            this.DataContext = Viewmodel = new ControlBarViewModel();
+        }
+
+        private void Bell_icon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserProfile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shoping_card_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Mylesson_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MyCourse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
