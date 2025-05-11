c9a224ae3797a08b359b1f67d79462beb54e0219
Noahsa-HiNg <102230149@sv1.dut.udn.vn>
2025-05-11 14:39:46 +0700

    Update database

--- GUI/ViewModel/ListCourseViewModel.cs
+++ GUI/ViewModel/ListCourseViewModel.cs
@@ -15,59 +15,59 @@ namespace GUI.ViewModel
         public ObservableCollection<CourseCategory> Courses { get; set; }
         public ListCourseViewModel()
         {
-            //CourseManagementBLL courseManagementBLL = new CourseManagementBLL();
-            //CourseCategory[] courseCategories = courseManagementBLL.ShowAllCourse();
-            //Courses = new ObservableCollection<CourseCategory>(courseCategories);
-           
-                Courses = new ObservableCollection<CourseCategory>
-                {
-                   new CourseCategory
-                   {
-                       Category_ID = "C001",
-                       Name = "Khóa học C#",
-                       Description = "Học lập trình C# từ cơ bản đến nâng cao",
-                       Price = 199.99m,
-                       Created_By = 1,
-                       Created_By_Role = "Admin",
-                       Update_By = 2,
-                       Update_By_Role = "Editor",
-                       Created_Date = DateTime.Now.AddMonths(-2),
-                       Updated_Date = DateTime.Now.AddMonths(-1),
-                       Delete_At = DateTime.MinValue,
-                       Is_Delete = false
-                   },
-                   new CourseCategory
-                   {
-                       Category_ID = "C002",
-                       Name = "Khóa học WPF",
-                       Description = "Xây dựng ứng dụng desktop với WPF",
-                       Price = 149.99m,
-                       Created_By = 1,
-                       Created_By_Role = "Admin",
-                       Update_By = 3,
-                       Update_By_Role = "Moderator",
-                       Created_Date = DateTime.Now.AddMonths(-3),
-                       Updated_Date = DateTime.Now.AddMonths(-2),
-                       Delete_At = DateTime.MinValue,
-                       Is_Delete = false
-                   },
-                   new CourseCategory
-                   {
-                       Category_ID = "C003",
-                       Name = "Khóa học ASP.NET",
-                       Description = "Phát triển web với ASP.NET Core",
-                       Price = 249.99m,
-                       Created_By = 2,
-                       Created_By_Role = "Editor",
-                       Update_By = 3,
-                       Update_By_Role = "Moderator",
-                       Created_Date = DateTime.Now.AddMonths(-4),
-                       Updated_Date = DateTime.Now.AddMonths(-3),
-                       Delete_At = DateTime.MinValue,
-                       Is_Delete = false
-                   }
-                };
-            
+            CourseManagementBLL courseManagementBLL = new CourseManagementBLL();
+            CourseCategory[] courseCategories = courseManagementBLL.ShowAllCourse();
+            Courses = new ObservableCollection<CourseCategory>(courseCategories);
+
+            //Courses = new ObservableCollection<CourseCategory>
+            //{
+            //   new CourseCategory
+            //   {
+            //       Category_ID = "C001",
+            //       Name = "Khóa học C#",
+            //       Description = "Học lập trình C# từ cơ bản đến nâng cao",
+            //       Price = 199.99m,
+            //       Created_By = 1,
+            //       Created_By_Role = "Admin",
+            //       Update_By = 2,
+            //       Update_By_Role = "Editor",
+            //       Created_Date = DateTime.Now.AddMonths(-2),
+            //       Updated_Date = DateTime.Now.AddMonths(-1),
+            //       Delete_At = DateTime.MinValue,
+            //       Is_Delete = false
+            //   },
+            //   new CourseCategory
+            //   {
+            //       Category_ID = "C002",
+            //       Name = "Khóa học WPF",
+            //       Description = "Xây dựng ứng dụng desktop với WPF",
+            //       Price = 149.99m,
+            //       Created_By = 1,
+            //       Created_By_Role = "Admin",
+            //       Update_By = 3,
+            //       Update_By_Role = "Moderator",
+            //       Created_Date = DateTime.Now.AddMonths(-3),
+            //       Updated_Date = DateTime.Now.AddMonths(-2),
+            //       Delete_At = DateTime.MinValue,
+            //       Is_Delete = false
+            //   },
+            //   new CourseCategory
+            //   {
+            //       Category_ID = "C003",
+            //       Name = "Khóa học ASP.NET",
+            //       Description = "Phát triển web với ASP.NET Core",
+            //       Price = 249.99m,
+            //       Created_By = 2,
+            //       Created_By_Role = "Editor",
+            //       Update_By = 3,
+            //       Update_By_Role = "Moderator",
+            //       Created_Date = DateTime.Now.AddMonths(-4),
+            //       Updated_Date = DateTime.Now.AddMonths(-3),
+            //       Delete_At = DateTime.MinValue,
+            //       Is_Delete = false
+            //   }
+            //};
+
         }
     }
 }
